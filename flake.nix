c7027c32c12f7badc4e52811b4cf5db21376ffaf
Tri R. Utomo <tri.r.utomo@gmail.com>
2026-01-17 07:09:26 +0700

    style: clean up flake.nix inputs - condensed single-line format

--- flake.nix
+++ flake.nix
@@ -1,5 +1,4 @@
 # https://wiki.nixos.org/wiki/Flakes
-# https://nix.dev/manual/nix/stable/command-ref/new-cli/nix3-flake
 
 {
   nixConfig = {
@@ -15,89 +14,36 @@
     ];
   };
 
-  # Inputs organized by category (from src/flake/inputs.nix pattern)
   inputs = {
-    # Core infrastructure
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-
-    # System configuration
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Pre-commit hooks
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Desktop environment
+    home-manager = { url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
+    sops-nix = { url = "github:Mic92/sops-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
     hyprland.url = "github:hyprwm/Hyprland";
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
-    stylix = {
-      url = "github:danth/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Secrets management
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Applications
-    vicinae-extensions = {
-      url = "github:vicinaehq/extensions";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
-    spicetify-nix = {
-      url = "github:Gerg-L/spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixcord = {
-      url = "github:kaylorben/nixcord";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    youtube-music = {
-      url = "github:h-banii/youtube-music-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    apple-fonts = {
-      url = "github:Lyndeno/apple-fonts.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    antigravity = {
-      url = "github:jacopone/antigravity-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    hyprland-plugins = { url = "github:hyprwm/hyprland-plugins"; inputs.hyprland.follows = "hyprland"; };
+    stylix = { url = "github:danth/stylix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    zen-browser = { url = "github:0xc000022070/zen-browser-flake"; inputs.nixpkgs.follows = "nixpkgs"; inputs.home-manager.follows = "home-manager"; };
+    spicetify-nix = { url = "github:Gerg-L/spicetify-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    nixcord = { url = "github:kaylorben/nixcord"; inputs.nixpkgs.follows = "nixpkgs"; };
+    youtube-music = { url = "github:h-banii/youtube-music-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    vicinae-extensions = { url = "github:vicinaehq/extensions"; inputs.nixpkgs.follows = "nixpkgs"; };
+    firefox-addons = { url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons"; inputs.nixpkgs.follows = "nixpkgs"; };
+    apple-fonts = { url = "github:Lyndeno/apple-fonts.nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    pre-commit-hooks = { url = "github:cachix/pre-commit-hooks.nix"; inputs.nixpkgs.follows = "nixpkgs"; };
+    antigravity = { url = "github:jacopone/antigravity-nix"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs =
-    { self, flake-parts, ... }@inputs:
-    let
-      identity = import ./cfg/identity.nix;
-      utils = import ./src/util { inherit inputs self; };
-    in
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import ./src/flake/systems.nix;
-      perSystem = import ./src/flake/outputs/workspace.nix { inherit inputs self; };
-      flake = (import ./src/flake/outputs/artifacts.nix {
-        inherit inputs identity utils self;
-      }) // (import ./src/flake/modules.nix);
-    };
+  outputs = { self, flake-parts, ... }@inputs:
+  let
+    identity = import ./cfg/identity.nix;
+    utils = import ./src/util { inherit inputs self; };
+  in
+  flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import ./src/flake/systems.nix;
+    perSystem = import ./src/flake/outputs/workspace.nix { inherit inputs self; };
+    flake = (import ./src/flake/outputs/artifacts.nix {
+      inherit inputs identity utils self;
+    }) // (import ./src/flake/modules.nix);
+  };
 }
